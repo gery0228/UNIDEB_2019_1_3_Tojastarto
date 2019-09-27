6939f7e1a9d94da4e438b5e9921a1359483f71e7
gery0228 <gery980228@gmail.com>
2019-09-27 23:09:40 +0200

    sample package és sample fxml fájl refaktprálása (BankATM, login.fxml) és Maven keresztrendszer hozzáadása a programhoz.

--- Projectlabor/src/main/java/BankATM/Main.java
+++ Projectlabor/src/main/java/BankATM/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package BankATM;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/login.fxml"));
         primaryStage.setTitle("Bejelentkezés");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
--- Projectlabor/src/main/java/BankATM/Model.java
+++ Projectlabor/src/main/java/BankATM/Model.java
@@ -1,4 +1,4 @@
-package sample;
+package BankATM;
 
 public class Model {
 }
