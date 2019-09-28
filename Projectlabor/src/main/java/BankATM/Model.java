22186e12a222277b862dc810840607c42e84e2a3
ktomi26 <ktomi200@gmail.com>
2019-09-28 21:17:48 +0200

    Model osztály int, Stringek (balance, username, password) és getter, setter metódusok létrehozva hozzájuk.

--- Projectlabor/src/main/java/BankATM/Model.java
+++ Projectlabor/src/main/java/BankATM/Model.java
@@ -1,4 +1,38 @@
 package BankATM;
 
 public class Model {
+
+
+	private int balance;
+    private String username;
+    private String password;
+
 }
+
+   public Model(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
