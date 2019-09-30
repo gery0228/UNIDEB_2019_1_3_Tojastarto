e7c8e6e62446a49d884136292fee828886c9ed59
gery0228 <gery980228@gmail.com>
2019-09-30 02:44:10 +0200

    Tesztek írása a pénzösszeg felvételének, pénzfeltöltésének vizsgálatára. maven-surefire-plugin plugin hozzáadása a pom-hoz.

--- Projectlabor/src/test/java/BankATM/ControllerTest.java
+++ Projectlabor/src/test/java/BankATM/ControllerTest.java
@@ -11,8 +11,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
+    int balance = 2000;
+    String a = "1000";
+    String ures = "";
+    String minusz = "-200";
+    String nagy = "3000";
+
+
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
@@ -22,9 +30,41 @@ class ControllerTest {
     @Test
     void deposit() {
 
-        File file = new File("/Users/ktomi/Desktop/UNIDEB_2019_1_3_Tojastarto/Projectlabor/balance.json");
+        File file = new File("/balance.json");
         assertTrue(file.exists());
 
+    }
+
+    @Test
+    void equal() {
+        if (a.isEmpty() || Integer.parseInt(a) < 0 || (balance - Integer.parseInt(a) < 0)) {
+            assertEquals(1000, balance);
+        }
+    }
+
+    @Test
+    void nagy() {
+        assertTrue(nagy.isEmpty() || Integer.parseInt(nagy) < 0 || (balance - Integer.parseInt(nagy) < 0));
+    }
+
+    @Test
+    void notgood() {
+        assertTrue(ures.isEmpty() || Integer.parseInt(ures) < 0 || (balance - Integer.parseInt(ures) < 0));
+    }
+
+    @Test
+    void minusz() {
+        assertTrue(minusz.isEmpty() || Integer.parseInt(minusz) < 0 || (balance - Integer.parseInt(minusz) < 0));
+    }
+
+    @Test
+    void empty() {
+        assertTrue(ures.isEmpty() || Integer.parseInt(a) < 0);
+    }
+
+    @Test
+    void negative() {
+        assertTrue(a.isEmpty() || Integer.parseInt(minusz) < 0);
     }
 
 }
